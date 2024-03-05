141fe48374c9b1ab838170598345ff9f9d40e465
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-03-05 00:52:46 -0500

    Intento 5

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -18,11 +18,11 @@ public class Grupo {
 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         Persona[] personas = new Persona[cantidadEstudiantes];
-        this.estudiantes = personas.estudiantes;
-        this.profesor = personas.profesor;
-        this.asignatura = personas.asignatura;
-        this.codigo = personas.codigo;
-        this.horario = personas.horario;
+        this.estudiantes = personas;
+        this.profesor = profesor;
+        this.asignatura = asignatura;
+        this.codigo = codigo;
+        this.horario = horario;
     }
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
